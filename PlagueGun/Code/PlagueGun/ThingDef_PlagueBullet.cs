637ba89aa4b7e2ae8612bdd20cdaab64a560ce1a
Austin George <austinageorge@gmail.com>
2022-08-13 23:18:49 -0500

    Adding Code

---
+++ PlagueGun/Code/PlagueGun/ThingDef_PlagueBullet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace Togie.PlagueGun
+{
+    public class ThingDef_PlagueBullet : ThingDef
+    {
+        public float AddHediffChance = 0.67f; //The default chance of adding a hediff.
+        public HediffDef HediffToAdd;
+
+        //public override void ResolveReferences()
+        //{
+        //    base.ResolveReferences();
+        //    HediffToAdd = HediffDefOf.Plague;
+        //}
+    }
+}
